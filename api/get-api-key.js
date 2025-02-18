6c2aa2dfd6e6de6681b0fa2090ed671373f51585
Vibhav Yadav <144496436+Vibhav-y@users.noreply.github.com>
2025-02-18 22:19:23 +0530

    Update get-api-key.js

--- api/get-api-key.js
+++ api/get-api-key.js
@@ -18,7 +18,7 @@ const validMachineIds = {
     'D7-1B-81-C9-BD-D9': 'AIzaSyCqfTBkRhGHZF62Kc8ROSWvxmutb_4Abvo', //SAKCHI 3
     '50-5A-65-F6-04-B7': 'AIzaSyCqfTBkRhGHZF62Kc8ROSWvxmutb_4Abvo', //SHREY
     'B4-8C-9D-5E-2A-97': 'AIzaSyCqfTBkRhGHZF62Kc8ROSWvxmutb_4Abvo',
-    
+    '2C-8D-B1-51-8D-CB': 'AIzaSyCqfTBkRhGHZF62Kc8ROSWvxmutb_4Abvo', //SAKCHI 4
     
     // '14-D4-24-34-99-91': 'AIzaSyCqfTBkRhGHZF62Kc8ROSWvxmutb_4Abvo',
     // Add more machine IDs and their associated API keys here

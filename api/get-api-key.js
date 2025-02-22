53f085fd480404ae15f5ca5507b1a684a648e614
Vibhav Yadav <144496436+Vibhav-y@users.noreply.github.com>
2025-02-22 15:03:43 +0530

    Update get-api-key.js

--- api/get-api-key.js
+++ api/get-api-key.js
@@ -9,8 +9,8 @@ export default function handler(req, res) {
         const { machine_id } = req.body;
 
         if (validMachineIds[machine_id]) {
-            res.status(200).json(api);
-            // res.status(200).json({ api_key: api });
+            // res.status(200).json(api);
+            res.status(200).json({ api_key: api });
         } else {
             res.status(401).json({ error: 'Failed to get verification key. Check Internet, license or contact us.', error_code: 401 });
         }

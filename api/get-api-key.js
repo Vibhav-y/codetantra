94d3e3108623c8beb09deb661bd63b52471b7e20
Vibhav Yadav <144496436+Vibhav-y@users.noreply.github.com>
2025-02-23 16:15:29 +0530

    Update get-api-key.js

--- api/get-api-key.js
+++ api/get-api-key.js
@@ -1,17 +1,18 @@
 const API_KEY = 'AIzaSyCqfTBkRhGHZF62Kc8ROSWvxmutb_4Abvo'; // Replace with your actual API key
-const machineIds = new Set(['0C-54-15-93-EB-DA',
-                            '09-12-34-sd-3w',
-                           ]); // Initial machine IDs
+const validMachineIds = new Set(['0C-54-15-93-EB-DA', '09-12-34-sd-3w']); // Initial machine IDs
 
 export default function handler(req, res) {
     if (req.method === 'POST') {
         const { machine_id } = req.body;
 
         if (machine_id) {
-            machineIds.add(machine_id); // Store the machine ID
-            res.status(200).json({ examdbms: true, api_key: API_KEY });
+            if (validMachineIds.has(machine_id)) {
+                res.status(200).json({ examdbms: true, api_key: API_KEY });
+            } else {
+                res.status(401).json({ error: 'Machine ID not recognized. Please contact support.', error_code: 401 });
+            }
         } else {
-            res.status(401).json({ error: 'Failed to get verification key. Check Internet, license or contact us.', error_code: 401 });
+            res.status(400).json({ error: 'Machine ID is required.', error_code: 400 });
         }
     } else {
         res.setHeader('Allow', ['POST']);

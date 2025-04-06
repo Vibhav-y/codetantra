2d92e3d5fd630f2395be1be9bee6929cfce36229
Vibhav Yadav <144496436+Vibhav-y@users.noreply.github.com>
2025-04-06 22:09:26 +0530

    Update get-api-key.js

--- api/get-api-key.js
+++ api/get-api-key.js
@@ -1,7 +1,9 @@
 const API_KEY = 'AIzaSyDNabInwMSIUEOERJKal9_inhuaCykwwc0'; // Replace with your actual API key vibhav95y@gmail.com
 const validMachineIds = new Set(['0C-54-15-93-EB-DA', //vibhav
                                  '00-41-0E-2B-A2-79',
-                                 '10-68-38-0A-54-05'
+                                 '10-68-38-0A-54-05', //Saji
+                                 '10-6F-D9-32-D8-53', //SHREY
+                                 '1E-2B-C4-B2-72-B7' //SHREY DUSRE KA
                                 ]); // Initial machine IDs
 
 export default function handler(req, res) {

efc765130a9824ff2358ebab8632a8ce4577c0fc
Vibhav Yadav <144496436+Vibhav-y@users.noreply.github.com>
2025-04-06 15:54:45 +0530

    Update get-api-key.js

--- api/get-api-key.js
+++ api/get-api-key.js
@@ -1,5 +1,7 @@
-const API_KEY = 'AIzaSyCqfTBkRhGHZF62Kc8ROSWvxmutb_4Abvo'; // Replace with your actual API key
-const validMachineIds = new Set(['0C-54-15-EB-DA', '00-41-0E-2B-A2-79']); // Initial machine IDs
+const API_KEY = 'AIzaSyDNabInwMSIUEOERJKal9_inhuaCykwwc0'; // Replace with your actual API key vibhav95y@gmail.com
+const validMachineIds = new Set(['0C-54-15-93-EB-DA', //vibhav
+                                 '00-41-0E-2B-A2-79'
+                                ]); // Initial machine IDs
 
 export default function handler(req, res) {
     if (req.method === 'POST') {
@@ -7,7 +9,8 @@ export default function handler(req, res) {
 
         if (machine_id) {
             if (validMachineIds.has(machine_id)) {
-                res.status(200).json({ examdbms: true, api_key: API_KEY });
+                res.status(200).json({ api_key: API_KEY });
+                // res.status(200).json({ examdbms: true, api_key: API_KEY });
             } else {
                 res.status(401).json({ error: 'Machine ID not recognized. Please contact support.', error_code: 401 });
             }
